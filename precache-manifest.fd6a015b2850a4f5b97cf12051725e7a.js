619dfe69a98b126785f62b1ce7b8c480528ddfd0
Avnsh Yadav <avnishyadav2561995@gmail.com>
2024-09-03 06:34:13 +0000

    Updates

---
+++ precache-manifest.fd6a015b2850a4f5b97cf12051725e7a.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f94a8e4b3737c60b190d5f91ff1ed03",
+    "url": "/healing-homeopathy/web-app/index.html"
+  },
+  {
+    "revision": "f40a1a303036b85d1ebd",
+    "url": "/healing-homeopathy/web-app/static/css/2.2d898be2.chunk.css"
+  },
+  {
+    "revision": "81e5f39bac8cbe36b7fb",
+    "url": "/healing-homeopathy/web-app/static/css/main.1897d9e0.chunk.css"
+  },
+  {
+    "revision": "f40a1a303036b85d1ebd",
+    "url": "/healing-homeopathy/web-app/static/js/2.cc5eff9d.chunk.js"
+  },
+  {
+    "revision": "ff148c49a4f52b8bde46ef5bbf783ba8",
+    "url": "/healing-homeopathy/web-app/static/js/2.cc5eff9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e5f39bac8cbe36b7fb",
+    "url": "/healing-homeopathy/web-app/static/js/main.181f1010.chunk.js"
+  },
+  {
+    "revision": "8a1c141287ae56d36052",
+    "url": "/healing-homeopathy/web-app/static/js/runtime-main.9d4fc89f.js"
+  },
+  {
+    "revision": "a2292a6db07c281e85b59926f00b4ab8",
+    "url": "/healing-homeopathy/web-app/static/media/about-cover.a2292a6d.jpg"
+  },
+  {
+    "revision": "e43cbfeb30dc315c089d5ae87efdc481",
+    "url": "/healing-homeopathy/web-app/static/media/approach-image.e43cbfeb.jpg"
+  },
+  {
+    "revision": "3909b17b303fd94f45b31aa697b2ff76",
+    "url": "/healing-homeopathy/web-app/static/media/certification1.3909b17b.jpg"
+  },
+  {
+    "revision": "d855adff82c922c00bf0b48593625a73",
+    "url": "/healing-homeopathy/web-app/static/media/chronic.d855adff.jpg"
+  },
+  {
+    "revision": "9d6326f7f37f4891e1debcebedcff3c5",
+    "url": "/healing-homeopathy/web-app/static/media/consultation.9d6326f7.jpg"
+  },
+  {
+    "revision": "d8c1885165916b30b858ee0d1553010c",
+    "url": "/healing-homeopathy/web-app/static/media/contact-cover.d8c18851.jpg"
+  },
+  {
+    "revision": "bcc29c4296c22546ed433df807e33e9d",
+    "url": "/healing-homeopathy/web-app/static/media/contact-image.bcc29c42.jpg"
+  },
+  {
+    "revision": "14eb647c08a9a965f166e1db87e9d7c9",
+    "url": "/healing-homeopathy/web-app/static/media/customer1.14eb647c.jpg"
+  },
+  {
+    "revision": "fe79a95a781a1fff42c1bbd646d5ea50",
+    "url": "/healing-homeopathy/web-app/static/media/delivery.fe79a95a.jpg"
+  },
+  {
+    "revision": "1d51916aaaf8e45bc24e718efc1a2fe8",
+    "url": "/healing-homeopathy/web-app/static/media/hero-image.1d51916a.jpg"
+  },
+  {
+    "revision": "2c5864266d2514a1645fd009915b4316",
+    "url": "/healing-homeopathy/web-app/static/media/homepage-cover.2c586426.jpg"
+  },
+  {
+    "revision": "6044ceb505d7aa28f666118c763d2953",
+    "url": "/healing-homeopathy/web-app/static/media/immune.6044ceb5.jpg"
+  },
+  {
+    "revision": "fcea9250cd7fc124ddc6f0a1c5f0b6fb",
+    "url": "/healing-homeopathy/web-app/static/media/login-here1.fcea9250.jpg"
+  },
+  {
+    "revision": "4050cab8c560fc3f0833607f8467745c",
+    "url": "/healing-homeopathy/web-app/static/media/logo.4050cab8.svg"
+  },
+  {
+    "revision": "9baed7f6d04e9beda33b623694a8e85b",
+    "url": "/healing-homeopathy/web-app/static/media/medicine-delivery-cover.9baed7f6.jpg"
+  },
+  {
+    "revision": "8e339e564762d637fc19b80109322d4a",
+    "url": "/healing-homeopathy/web-app/static/media/mental.8e339e56.jpg"
+  },
+  {
+    "revision": "8ac9c390c913a8fbc17d385602cb071c",
+    "url": "/healing-homeopathy/web-app/static/media/mission-image.8ac9c390.jpg"
+  },
+  {
+    "revision": "be7cdd88db5901de1ea587cbc7529566",
+    "url": "/healing-homeopathy/web-app/static/media/newsletter.be7cdd88.jpg"
+  },
+  {
+    "revision": "554b6501515d87bcb9e45ce00713c86e",
+    "url": "/healing-homeopathy/web-app/static/media/remedies.554b6501.jpg"
+  },
+  {
+    "revision": "4e01f77f73361ee4c4cbcce6e73fa5c8",
+    "url": "/healing-homeopathy/web-app/static/media/services-cover.4e01f77f.jpg"
+  },
+  {
+    "revision": "5e6835064dc90fdaf4e1f755edf90736",
+    "url": "/healing-homeopathy/web-app/static/media/story-image.5e683506.jpg"
+  },
+  {
+    "revision": "1c98a4a1c3a039b12f0d3fb9856dbe91",
+    "url": "/healing-homeopathy/web-app/static/media/team-member1.1c98a4a1.jpg"
+  },
+  {
+    "revision": "d87702fb656857ac088384619f19c7fa",
+    "url": "/healing-homeopathy/web-app/static/media/wellness.d87702fb.jpg"
+  }
+]);
